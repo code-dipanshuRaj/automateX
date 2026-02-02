d8c6bfff2bdd1cbe7cc59c96d85a2088fea59c17
adiwashere <aadityakashyapss@gmail.com>
2026-02-02 10:00:24 +0530

    ignore env and added chat history

--- pr1.py
+++ pr1.py
@@ -14,11 +14,12 @@ model = ChatHuggingFace(llm=llm)
 
 #chat historty
 chat_history = [
-    
+  
 ]
 
 
 
+MAX_HISTORY = 20
 
 while True:
     user_input = input("You: ")
@@ -26,11 +27,12 @@ while True:
     if user_input == "exit":
         break 
     chat_history.append(HumanMessage(content=user_input))
+    chat_history = chat_history[-MAX_HISTORY:] #keep only last 20 messages
      
     result= model.invoke(chat_history)
     chat_history.append(AIMessage(content=result.content))
+    chat_history = chat_history[-MAX_HISTORY:]
+
     print("AI:", result.content)
 
 print("Chat ended.")
-for msg in chat_history:
-    print(msg.content)
